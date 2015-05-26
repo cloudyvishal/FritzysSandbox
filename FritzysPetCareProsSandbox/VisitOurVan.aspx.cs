40940bf8e2ae7c37996c64dae961abc097fbe231
Vishal <cloudyvishal@outlook.com>
2015-05-27 02:39:59 +0530

    Visit Van

--- FritzysPetCareProsSandbox/VisitOurVan.aspx.cs
+++ FritzysPetCareProsSandbox/VisitOurVan.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using BCL.Utility.Contents;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,49 +13,7 @@ namespace FritzysPetCareProsSandbox
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            bool Ismobile = false;
-            string[] mobiles =
-            new[]
-                {
-                    "midp","android","j2me", "avant", "docomo", 
-                    "novarra", "palmos", "palmsource", 
-                     "opwv", "chtml","320x480",
-                    "pda", "windows ce", "mmp/", 
-                    "blackberry", "mib/", "symbian", 
-                    "wireless", "nokia", "hand", "mobi", 
-                    "phone", "cdm", "up.b", "audio", 
-                    "SIE-", "SEC-", "samsung", "HTC", 
-                    "mot-", "mitsu", "sagem", "sony"
-                    , "alcatel", "lg", "eric", "vx", 
-                     "philips", "mmm", "xx", 
-                    "panasonic", "sharp", "wap", "sch",
-                    "rover", "pocket", "benq", "java", 
-                    "pt", "pg", "vox", "amoi", 
-                    "bird", "compal", "kg", "voda",
-                    "sany", "kdd", "dbt", "sendo", 
-                    "sgh", "gradi", "jb", "dddi", 
-                    "moto", "iphone"
-                };
-            foreach (string s in mobiles)
-            {
-                if (Request.ServerVariables["HTTP_USER_AGENT"].ToLower().Contains(s.ToLower()))
-                {
-                    Ismobile = true;
-                }
-            }
-
-            if (Ismobile.Equals(true))
-            {
-                HttpContext.Current.Response.Status = "301 Moved Permanently";
-                HttpContext.Current.Response.AddHeader("Location",
-                Request.Url.ToString().Replace(Request.Url.ToString(), ("http://localhost:50372/fritzymobile/MB_visit-our-van.aspx").ToLower()));
-
-            }
-
-            else
-            {
-                litContent.Text = ContentManager.GetFileContentView(ContentManager.GetPhysicalPath(SessionManagement.HomePath.ToString() + "StoreData/StaticeContent/VisitOurVan.htm"));
-            }
+            litContent.Text = ContentManager.GetFileContentView(ContentManager.GetPhysicalPath(ConfigurationManager.AppSettings["HomePathValue"].ToString() + "StoreData/StaticeContent/VisitOurVan.htm"));
         }
     }
 }
