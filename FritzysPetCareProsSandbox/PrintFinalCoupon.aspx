79d4849945ee4a62666695113b253eced869740e
TAAPAS Technology LLP <cloudyvishal@outlook.com>
2015-12-11 22:50:41 +0530

    PrintCoupon Page Added

---
+++ FritzysPetCareProsSandbox/PrintFinalCoupon.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PrintFinalCoupon.aspx.cs" Inherits="FritzysPetCareProsSandbox.PrintFinalCoupon" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Print Final Coupon</title>
+    <script lang="javascript" type="text/javascript">
+        function PrintIt() {
+            window.print();
+            window.close();
+            return false;
+        }
+    </script>
+
+
+    <link href="Styles.css" type="text/css" rel="Stylesheet" />
+</head>
+<body onload="PrintIt();">
+    <form id="form1" runat="server">
+        <div>
+            <div style="width: 700px; height: 300px; background: #decfba; margin: 30px auto; text-align: center; padding: 20px;">
+                <asp:Image ID="ImgGift" runat="server" border="0" alt="Print Coupon" />
+                <br />
+
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FritzysPetCareProsSandbox/PrintFinalCoupon.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Web;
+using System.Data;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using FritzysPetCarePros.Controls;
+
+using BCL.Admin.BannerMgr;
+using BCL.Utility.Contents;
+
+namespace FritzysPetCareProsSandbox
+{
+    public partial class PrintFinalCoupon : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string ImgName = "";
+            string ImagePath;
+            try
+            {
+                ImgName = Request.QueryString["CouponID"].ToString();
+            }
+            catch (Exception ex)
+            {
+            }
+            Banners newObj = new Banners();
+            DataSet ds = newObj.GetBannerImageNameandpath(Convert.ToInt32(Request.QueryString["CouponID"].ToString()));
+            ImagePath = Session["HomePath"] + "StoreData/BannerNew/" + ds.Tables[0].Rows[0]["BannerName"].ToString();
+
+            string imagepath = ImagePath;
+            string fulpath = ContentManager.GetPhysicalPath(imagepath);
+            if (System.IO.File.Exists(fulpath))
+            {
+                ImgGift.ImageUrl = imagepath;
+            }
+        }
+    }
+}
